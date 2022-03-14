6693509774b9aed9f11b0d16bb3b30a1a478ca71
verissimo francis <verissimo francis>
2022-03-14 14:14:43 +0100

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <React.StrictMode />,
   document.getElementById('root'),
 );
 
